78b37e1c14e639aea7a087f4881f2dfa7e347986
papoi1 <mikelxabier@gmail.com>
2018-01-23 19:16:20 +0100

    remove one test. modify show.html.erb

--- spec/features/katas_spec.rb
+++ spec/features/katas_spec.rb
@@ -2,19 +2,25 @@ require 'rails_helper'
 require 'capybara'
 
 describe 'Kata' do
-  it 'can be read' do
-    kata_title = 'Kata title'
-    kata_description = 'Kata description'
-    kata = Kata.new(
-      title: kata_title,
-      description: kata_description
-    )
-    kata.save
+
+  it 'can be read(two katas)' do
+    kata_title = 'second title'
+    kata_description = 'second description'
+
+    create_kata
+    create_kata(title: kata_title, description: kata_description)
 
     visit root_path
     click_on(kata_title)
 
     expect(page).to have_content(kata_title)
     expect(page).to have_content(kata_description)
+  end
+
+  def create_kata(title: 'kata title', description: 'kata description')
+    kata = Kata.new(title: title, description: description)
+    kata.save
+
+    kata
   end
 end
