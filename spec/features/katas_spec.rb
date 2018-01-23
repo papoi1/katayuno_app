c2b88446973ded0bae30feba6f2397bac707051a
papoi1 <mikelxabier@gmail.com>
2018-01-23 20:27:59 +0100

    Added create function...

--- spec/features/katas_spec.rb
+++ spec/features/katas_spec.rb
@@ -17,6 +17,21 @@ describe 'Kata' do
     expect(page).to have_content(kata_description)
   end
 
+  it 'can be created' do
+    kata_title = 'second title'
+    kata_description = 'second description'
+
+    visit root_path
+    click_on('New Kata')
+    fill_in(:title, with: kata_title)
+    fill_in(:description, with: kata_description)
+    click_on('Save')
+
+    expect(page).to have_content(kata_title)
+    expect(page).to have_content(kata_description)
+
+  end
+
   def create_kata(title: 'kata title', description: 'kata description')
     kata = Kata.new(title: title, description: description)
     kata.save
