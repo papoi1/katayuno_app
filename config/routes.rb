1865be18787ece42d51e9697784b74edbf5db7a6
papoi1 <mikelxabier@gmail.com>
2018-01-24 20:01:10 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: 'katas#index'
+  root to: 'katas#index'
 
-get '/katas/:id' => 'katas#show', as: :kata
-get '/new/katas' => 'katas#new', as: :new_kata
-post '/create/katas' => 'katas#create', as: :create_katas
+  get '/katas/:id' => 'katas#show', as: :kata
+  get '/new/katas' => 'katas#new', as: :new_kata
+  post '/create/katas' => 'katas#create', as: :create_katas
+  get '/edit/katas/:id' => 'katas#edit', as: :edit_kata
+  patch '/katas/:id' => 'katas#update'
 
 end
--- spec/features/katas_spec.rb
+++ spec/features/katas_spec.rb
@@ -29,8 +29,28 @@ describe 'Kata' do
 
     expect(page).to have_content(kata_title)
     expect(page).to have_content(kata_description)
-
   end
+
+
+  it 'can be updated' do
+      kata_title = 'kata title2'
+      kata_description = 'kata description2'
+
+          kata_title2 = 'kata title2'
+          kata_description2 = 'kata description2'
+
+      create_kata(title: kata_title, description: kata_description)
+
+      visit root_path
+      click_on('Edit Kata')
+
+      fill_in(:kata_title, with: kata_title2)
+      fill_in(:kata_description, with: kata_description2)
+      click_on('Save')
+
+      expect(page).to have_content(kata_title2)
+      expect(page).to have_content(kata_description2)
+    end
 
   def create_kata(title: 'kata title', description: 'kata description')
     kata = Kata.new(title: title, description: description)
@@ -38,4 +58,5 @@ describe 'Kata' do
 
     kata
   end
+
 end
