5b75fb4a41460cd5372a22efd6ff5d09a64c725b
papoi1 <mikelxabier@gmail.com>
2018-02-06 20:31:18 +0100

    Belt...

--- app/models/belt.rb
+++ app/models/belt.rb
@@ -1,3 +1,12 @@
 class Belt < ApplicationRecord
   belongs_to :kata
+
+  def self.find_kata_id(belt)
+    find(belt).kata.id
+  end
+
+  def inline()
+      "#{name} - #{description}"
+  end
+
 end
--- spec/models/belt_spec.rb
+++ spec/models/belt_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
 describe 'Belt' do
+  before(:each) do
+    @kata = Kata.new(title:'title', description: 'sdf')
+    @kata.save
+    @belt = Belt.new(name: 'name', description: 'desc', kata_id: @kata.id)
+    @belt.save
+  end
   it 'belong to a kata' do
-    kata = Kata.new(title:'title', description: 'sdf')
-    kata.save
 
-    belt = Belt.new(
-      name: 'name',
-      description: 'desc',
-      kata_id: kata.id
-    )
-    belt.save
+    expect(@belt.kata).to eq(@kata)
+    expect(@kata.belts).to eq([@belt])
+  end
+  it 'retrieves its kata id' do
 
-    expect(belt.kata).to eq(kata)
-    expect(kata.belts).to eq([belt])
+    retrieved_kata_id = Belt.find_kata_id(@belt.id)
+
+    expect(retrieved_kata_id).to eq(@kata.id)
+  end
+  it 'retrieves title with description' do
+    expect(@belt.inline).to eq("#{@belt.name} - #{@belt.description}")
   end
 end
